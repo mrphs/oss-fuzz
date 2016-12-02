00c8a7baee93db469714adef95556ae267e803bb
Nima Fatemi <mrphs@riseup.net>
2016-12-01 20:44:23 -0500

    docker and build

---
+++ projects/tor/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+./autogen.sh
+# build without manpages
+
+./configure --disable-asciidoc
+
+make -j$(nproc) all
+
+# build fuzzers
+# e.g.
+# $CXX $CXXFLAGS -std=c++11 -Iinclude \
+#     /path/to/name_of_fuzzer.cc -o /out/name_of_fuzzer \
+#     -lfuzzer /path/to/library.a
